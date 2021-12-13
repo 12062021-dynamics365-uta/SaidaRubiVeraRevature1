55aa6303fea3dde7e9221f63ef5be708b704515d
Rubi <rubi@acuitusag.com>
2021-12-12 22:06:05 -0600

    Fridays HackerRank

---
+++ HankerRankDemos/MiniMaxSum-HackerRank/Project0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections
+using System.Collections.Concurrent
+
+
+namespace Project0
+{
+    class Vendor
+    {
+        //Store name and locations
+        Hashtable name_locations = new Hashtable();
+        //products and prices
+        Hashtable product_and_prices = new Hashtable()
+ ;       //Customers purchase history
+        ArrayList customer_purchase_history = new ArrayList
+       //stores purchase history
+        
+
+    }//EoC
+}//EoNS
