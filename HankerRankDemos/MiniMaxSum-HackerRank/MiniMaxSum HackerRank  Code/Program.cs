89bfd527a7584422bc3f9575ecd64e2786e35ec8
Rubi <rubi@acuitusag.com>
2021-12-13 20:26:58 -0600

    repushing minimax hacker rank code

---
+++ HankerRankDemos/MiniMaxSum-HackerRank/MiniMaxSum HackerRank  Code/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+
+    static void miniMaxSum(int[] arr)
+    {
+        Array.Sort(arr);
+        long min = arr.Take(4).Sum(item => (long)item);
+        long max = arr.Skip(1).Sum(item => (long)item);
+
+        Console.WriteLine($"{min} {max}");
+
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        miniMaxSum(arr);
+    }
+
+}
+
