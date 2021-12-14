ce6ac28b30339f5aa325079d12795d289a9d08c2
Rubi <rubi@acuitusag.com>
2021-12-13 20:32:43 -0600

    lost my inicial P0 :(

---
+++ Projects/Project0/Project0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections
+
+namespace Project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }//EofC
+
+    class
+
+
+
+}//EofN
