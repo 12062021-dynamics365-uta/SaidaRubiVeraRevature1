26be5e1d26ac19e6770402260b74d1ed6cdd82c5
Rubi <rubi@acuitusag.com>
2021-12-18 13:32:37 -0600

    Adding Thurs and Fri

---
+++ demos/JSON_Serialization practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace JSON_Serialization_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
