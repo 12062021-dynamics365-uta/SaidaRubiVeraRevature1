73b10b08e8e19af8d19b03ecf9dc83fee0a068cf
Rubi <rubi@acuitusag.com>
2021-12-20 10:48:47 -0600

    The Hurdle Race Hacker Rank Code

---
+++ Projects/Project0/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is where all your main code goes
+
+            //login and log out
+
+        }
+    }
+}
