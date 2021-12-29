a21ee03440d8d13a40f9b3c88ed5b288390424e4
Rubi Vera <95654441+Saida-Rubi-Vera@users.noreply.github.com>
2021-12-28 20:48:33 -0600

    Code Challenge Finished and Tested

--- c#codechallenge/3_DataTypeAndVariables/3_DataTypeAndVariables_StudentCopy/Program.cs
+++ c#codechallenge/3_DataTypeAndVariables/3_DataTypeAndVariables_StudentCopy/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace _3_DataTypeAndVariablesChallenge
 {
@@ -6,11 +7,33 @@ namespace _3_DataTypeAndVariablesChallenge
     {
         public static void Main(string[] args)
         {
-            //
-            //
-            // Insert code here.
-            //
-            //
+            byte b = 0;
+            sbyte sb = -128;
+            int t = -2147483648; //int32
+            uint ui = 4294967295;
+            short s = 32767; //int26
+            ushort us = 65535;
+            long l = -9223372036854775808; //int64
+            ulong ul = 18446744073709551615;
+            float f = 3238;
+            double d = 1.79769313486232;
+            char c = 'a';
+            bool bol = true;
+            object o = "|";
+            string str = "abc";
+            decimal dec = 3;
+      
+            string str1 = "I control text";
+            string numString = "123456";
+
+            List<object> list = new List<object>() {
+                b,sb,t,ui,s,us,l,ul,f,d,c,bol,o,str,dec
+            };
+
+            foreach (object i in list)
+            {
+                Console.WriteLine(i);
+            }
         }
 
         /// <summary>
@@ -25,7 +48,43 @@ namespace _3_DataTypeAndVariablesChallenge
         /// <returns></returns>
         public static string PrintValues(object obj)
         {
-            throw new NotImplementedException($"PrintValues() has not been implemented");
+            switch (Type.GetTypeCode(obj.GetType()))
+            {
+                case TypeCode.Byte:
+                    return "Data type => byte";
+                case TypeCode.SByte:
+                    return "Data type => sbyte";
+                case TypeCode.Int32:
+                    return "Data type => int";
+                case TypeCode.UInt32:
+                    return "Data type => uint";
+                case TypeCode.Int16:
+                    return "Data type => short";
+                case TypeCode.UInt16:
+                    return "Data type => ushort";
+                case TypeCode.Int64:
+                    return "Data type => long";
+                case TypeCode.UInt64:
+                    return "Data type => ulong";
+                case TypeCode.Single:
+                    return "Data type => float";
+                case TypeCode.Double:
+                    return "Data type => double";
+                case TypeCode.Char:
+                    return "Data type => char";
+                case TypeCode.Boolean:
+                    return "Data type => bool";
+                case TypeCode.Object:
+                    return "Data type => obj";
+                case TypeCode.String:
+                    return "Data type => string";
+                case TypeCode.Decimal:
+                    return "Data type => decimal";
+                default:
+                    return "nothing";
+            }
+
+            //throw new NotImplementedException($"PrintValues() has not been implemented");
         }
 
         /// <summary>
@@ -39,8 +98,13 @@ namespace _3_DataTypeAndVariablesChallenge
         /// <returns></returns>
         public static int? StringToInt(string numString)
         {
-            throw new NotImplementedException($"StringToInt() has not been implemented");
+            if (Int32.TryParse(numString, out int integer))
+                return integer;
+            else
+                return null;
+            //throw new NotImplementedException($"StringToInt() has not been implemented");
 
         }
+        
     }// end of class
 }// End of Namespace
