1cc73c033a3d4604377fdf727807d8739ddfb71c
Rubi Vera <95654441+Saida-Rubi-Vera@users.noreply.github.com>
2021-12-28 22:24:17 -0600

    Finished Code and Test Passed

--- c#codechallenge/4_Methods/4_Methods/Program.cs
+++ c#codechallenge/4_Methods/4_Methods/Program.cs
@@ -6,35 +6,95 @@ namespace _4_MethodsChallenge
     {
         public static void Main(string[] args)
         {
-            /**
-                YOUR CODE HERE.
-            **/
+            Console.WriteLine(GreetFriend(GetName()));
+            GetNumber();
+            GetAction();
         }
 
         public static string GetName()
         {
-            throw new NotImplementedException("GetName() is not implemented yet0");
+            Console.WriteLine("Howdy honey, what's your name?");
+            string name = Console.ReadLine();
+            return name;
+
+            //throw new NotImplementedException("GetName() is not implemented yet0");
         }
 
         public static string GreetFriend(string name)
         {
-            throw new NotImplementedException("GreetFriend() is not implemented yet");
+            return $"Hello, {name}. You are my friend.";
+            //throw new NotImplementedException("GreetFriend() is not implemented yet");
         }
 
         public static double GetNumber()
         {
-            throw new NotImplementedException("GetNumber() is not implemented yet");
+            double input;
+            bool repeat = true;
+
+            do
+            {
+                Console.WriteLine("Give me a number");
+                string number = Console.ReadLine();
+                if (double.TryParse(number, out input))
+                {
+                    repeat = false;
+                }
+                else
+                {
+                    Console.WriteLine("input not valid, try again.");
+                }
+            } while (repeat);
+
+            return input;
+            //throw new NotImplementedException("GetNumber() is not implemented yet");
 
         }
 
         public static int GetAction()
         {
-            throw new NotImplementedException("GetAction() is not implemented yet");
+            int input;
+            bool repeat = true;
+
+            do
+            {
+
+                Console.WriteLine(
+                        "Please selet an a operation:\n" +
+                        "1 - Add\n" +
+                        "2 - Subtract\n" +
+                        "3 - Multiply\n" +
+                        "4 - Divide\n"
+                        );
+                if (Int32.TryParse(Console.ReadLine(), out input))
+                {
+                    repeat = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, try again");
+                }
+            }
+            while (repeat);
+            return input;
+            //throw new NotImplementedException("GetAction() is not implemented yet");
         }
 
         public static double DoAction(double x, double y, int action)
         {
-            throw new NotImplementedException("DoAction() is not implemented yet");
+            switch(action)
+            {
+                case 1:
+                    return x + y;
+                case 2:
+                    return Math.Abs(x - y);
+                case 3:
+                    return x * y;
+                case 4:
+                    return x / y;
+                default:
+                    throw new FormatException("typeof(System.FormatExcption)");
+            }
+            //throw new NotImplementedException("DoAction() is not implemented yet");
         }
     }
 }
