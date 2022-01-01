9d30c36fddcb2381e22361da20790af03fcaff1a
Rubi Vera <95654441+Saida-Rubi-Vera@users.noreply.github.com>
2021-12-31 18:11:46 -0600

    Class Challenge Done

--- c#codechallenge/9_Classes/9_Classes/Human2.cs
+++ c#codechallenge/9_Classes/9_Classes/Human2.cs
@@ -6,6 +6,65 @@ namespace _9_ClassesChallenge
 {
     internal class Human2
     {
+        private string firstName = "Pat";
+        private string lastName = "Smyth";
+        private string eyeColor;
+        private int age;
+        private int weight;
 
-    }
-}
+
+        public int Weight {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (weight > 0 && weight < 900)
+                    weight = value;
+                else
+                    weight = 0;
+            }
+        }
+
+        public Human2()
+        {
+
+        }
+
+        public Human2(string fName, string lName, string eyecolor, int a)
+        {
+            firstName = fName;
+            lastName = lName;
+            eyeColor = eyecolor;
+            age = a;
+        }
+
+        public Human2(string fName, string lName, string eyecolor)
+        {
+            firstName=fName;
+            lastName=lName;
+            eyeColor=eyecolor;
+        }
+        public Human2(string fName,string lName, int a)
+        {
+            firstName=fName;
+            lastName=lName;
+            age=a;
+        }
+
+
+        public string AboutMe(string firstName, string lastName, string eyeColor, int age)
+        {
+            string baseLayer = $"My name is {firstName} {lastName}.";
+
+            if (age != 0)
+                baseLayer += $" My age is {age}.";
+            if (eyeColor != null)
+                baseLayer += $" My eye color is {eyeColor}.";
+            return baseLayer;
+        }
+
+
+    }//eofc
+}//eofn
