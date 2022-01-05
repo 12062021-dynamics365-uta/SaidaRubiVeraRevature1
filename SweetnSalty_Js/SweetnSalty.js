fbdd8d385f5cf5812a84e6fe2e81c33897ad5460
Rubi Vera <95654441+Saida-Rubi-Vera@users.noreply.github.com>
2022-01-04 20:48:17 -0600

    Creating JS SweetnSalty

---
+++ SweetnSalty_Js/SweetnSalty.js
@@ -0,0 +1,13 @@
+/*
+- Create a new branch called SweetnSaltyJs. Write all code on this branch.
+- This is the exact same requirements as SweetnSalty with some JS changes. 
+- Print the numbers 1 to 1000 to the console.
+- Print them in groups of 20 per line with one space separating each number. This will require string concatenation to print the 20-number strings to the console at a time. 
+- When the number is a multiple of three, print “sweet” instead of the number on the console.
+- If the number is a multiple of five, print “salty” (instead of the number) to the console.
+- For numbers which are multiples of three and five, print “sweet’nSalty” to the console (instead of the number).
+- After the numbers have all been printed, print out how many "sweet"'s, how many "salty"’s, and how many "sweet’nSalty"’s. These are three separate groups, so sweet’nSalty does not increment sweet or salty (and vice versa).
+- Include verbose commentary in the source code to tell me what each few lines are doing.
+- The coding Challenge is due by 10pm, Wednesday, 01.05.22.
+- Push the source code file called SweetnSalty.js to a directory called SweetnSalty_Js your repo.
+*/
